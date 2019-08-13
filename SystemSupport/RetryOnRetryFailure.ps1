8ad48111c0dd7021f00da8c4eca4e58d1f8d4afe
Feng Liu <feng3245@gmail.com>
2019-08-13 22:59:50 +0000

    Added retry for link failure retries.

---
+++ SystemSupport/RetryOnRetryFailure.ps1
@@ -0,0 +1,4 @@
+﻿if ((Get-ScheduledTask | Where TaskName -eq AutoMentorRetry ).State -eq "Ready" -AND (Get-ScheduledTask | Where TaskName -eq AutoMentorWeekly ).State -eq "Ready" -AND ((Get-ScheduledTaskInfo AutoMentorWeekly).LastTaskResult -eq 0) -AND (Get-Content -Path C:\AutoM\badlinks).Length -gt 0)
+{
+    Start-ScheduledTask -TaskName "AutoMentorRetry"
+}
